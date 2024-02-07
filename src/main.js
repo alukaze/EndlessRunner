19fdba3024860c3faff677fe7fec8ba5844a361f
Alan Lu <alu36@ucsc.edu>
2024-02-07 12:26:12 -0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+// Code Practice: Making a Scene
+// Name:
+// Date: 
+
+"use strict"
+
+let config = {
+    type: Phaser.AUTO,
+    width: 960,
+    height: 540
+}
+
+let game = new Phaser.Game(config)
